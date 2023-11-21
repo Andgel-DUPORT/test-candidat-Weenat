19b7df960145b8c7ba1b7ace8f2240300928c51b
Andgel DUPORT <andgel.duport@etu.univ-nantes.fr>
2023-11-21 20:52:38 +0100

    ajout d'un index, changement du processus de sauvegarde en base de données, différentes résolution de bug

--- app/main.py
+++ app/main.py
@@ -1,21 +1,22 @@
 import logging
-from enum import Enum
-
-import uvicorn
-from fastapi import FastAPI, Query
 import uuid
 from datetime import datetime
-from typing import Optional, List
-import requests
-from fastapi.encoders import jsonable_encoder
-from fastapi.responses import JSONResponse
+from enum import Enum
+from typing import List
 
-from pydantic import UUID4, BaseModel
-from sqlmodel import Field, SQLModel
 import pandas as pd
-import json
+import requests
+from fastapi import FastAPI, Query
+from pydantic import BaseModel
+from starlette.responses import FileResponse
 
 app = FastAPI()
+
+
+@app.get("/")
+async def read_index():
+    return FileResponse('app/resources/static/index.html')
+
 
 ##########ETL part#########
 url = "http://localhost:3000/measurements"
@@ -30,7 +31,7 @@ if request.status_code == 200:
             formatted_entry = {
                 "id": str(uuid.uuid4()),  # Generate a unique ID for each row
                 "datalogger": 1,  # We assume this data comes from the same sensor, so we set its datalogger ID to 1
-                "timestamp": timestamp,
+                "date": timestamp,
                 "precipitation": values["precip"],
                 "temperature": values["temp"],
                 "humidity": values["hum"]
@@ -39,40 +40,21 @@ if request.status_code == 200:
 
     # Creating a DataFrame with 'id' as the index
     df = pd.DataFrame(formatted_data)
+    df['date'] = pd.to_datetime(df['date'], unit='ms')
     df.set_index('id', inplace=True)
 
-    try:
-        # Read the existing CSV file
-        existing_df = pd.read_csv("database.csv", index_col='id', parse_dates=['timestamp'])
+    df.to_csv("database.csv")
+    logging.info(
+        "Database created successfully.")  # ici on se permet de créer la base de données purement et simplement.
+    # On pourrait ajouter une update de la base de données à chaque fois que le dataframe contient des données plus
+    # récentes que la plus récente donnée de la bdd
 
-        # Update or append rows based on the timestamp
-        for idx, row in df.iterrows():
-            if idx in existing_df.index:
-                # Update existing row
-                existing_df.loc[idx] = row
-            else:
-                # Append new row
-                existing_df = existing_df.append(row)
-
-        # Save the updated DataFrame to the CSV file
-        existing_df.to_csv("database.csv")
-
-        logging.info("Database updated successfully.")
-    except FileNotFoundError:
-        # If the file doesn't exist, save the current DataFrame to the CSV file
-        df.to_csv("database.csv")
-        logging.info("Database file not found. Created a new file.")
 else:
     logging.error(f"Error: {request.status_code}")
 
 
 ##########Schemas##########
 
-class MeasurementQueryParams(BaseModel):
-    since: datetime = Query(None, description="Filter by date and time. Ingestion date of returned records should be higher than the value provided. Format expected ISO-8601.")
-    before: datetime = Query(datetime.utcnow(), description="Filter by date and time. Ingestion date of returned records should be lower than the value provided. Default is now. Format expected ISO-8601.", allow_empty=True)
-    span: str = Query(None, description="Aggregates data given this parameter. Default value should be raw (meaning no aggregate).", enum=["day", "hour", "max"])
-    datalogger: str = Query(..., description="Filter by datalogger. This field is required. Should be an exact match of the datalogger id")
 
 class LabelField(Enum):
     PRECIPITATION = "precip"
@@ -97,65 +79,46 @@ class DataRecordAggregateResponse(BaseModel):
 def timescale_is_wrong(since, before):
     return since < before
 
+
 # Function to retrieve data from the CSV file
-def get_data_from_csv(query_params: MeasurementQueryParams):
+def get_data_from_csv(datalogger: str, before: datetime, since: datetime, span: str):
     try:
-        df = pd.read_csv("database.csv", index_col='id', parse_dates=['timestamp'])
-        filtered_df = df[(df['timestamp'] >= query_params.since) & (df['timestamp'] <= query_params.before) & (df['datalogger'] == query_params.datalogger)]
+        dataframe = pd.read_csv("database.csv", index_col='id')
+        dataframe['date'] = pd.to_datetime(dataframe['date'])
+        filtered_df = dataframe[(dataframe['date'] >= since) & (dataframe['date'] <= before) & (
+                dataframe['datalogger'] == datalogger)]
 
-        if query_params.span:
-            # Aggregate data based on the specified span
-            if query_params.span == "day":
-                aggregated_df = filtered_df.resample('D').mean()
-            elif query_params.span == "hour":
-                aggregated_df = filtered_df.resample('H').mean()
-            elif query_params.span == "max":
-                aggregated_df = filtered_df.resample('D').max()
-
-            # Reset index to make time_slot a separate column
-            aggregated_df.reset_index(inplace=True)
-
-            # Convert the DataFrame to a list of DataRecordAggregateResponse objects
-            result = [
-                DataRecordAggregateResponse(
-                    label=LabelField(label),
-                    time_slot=row['timestamp'],
-                    value=row[metric]
-                )
-                for label, metric in [('temp', 'temp'), ('hum', 'hum'), ('precip', 'precip')]
-                for _, row in aggregated_df.iterrows()
-            ]
-        else:
-            # Convert the DataFrame to a list of DataRecordResponse objects
-            result = [
-                DataRecordResponse(
-                    label=LabelField(row['label']),
-                    measured_at=row['timestamp'],
-                    value=row['value']
-                )
-                for _, row in filtered_df.iterrows()
-            ]
+        # Convert the DataFrame to a list of DataRecordResponse objects
+        result = [
+            DataRecordResponse(
+                label=LabelField(row['label']),
+                measured_at=row['date'],
+                value=row['value']
+            )
+            for _, row in filtered_df.iterrows()
+        ]
 
         return result
     except FileNotFoundError:
         return []  # Return an empty list if the file is not found
 
 
-
 ##########API part#########
 
 # Load the CSV file into a DataFrame
 try:
-    df = pd.read_csv("database.csv", index_col='id', parse_dates=['timestamp'])
+    df = pd.read_csv("database.csv", index_col='id', parse_dates=['date'])
 except FileNotFoundError:
-    df = pd.DataFrame(columns=['timestamp', 'precip', 'temp', 'hum'])
+    df = pd.DataFrame(columns=['date', 'precip', 'temp', 'hum'])
 
 
 @app.get("/api/summary", response_model=List[DataRecordAggregateResponse])
-async def api_fetch_data_aggregates(query_params: MeasurementQueryParams):
-    return get_data_from_csv(query_params)
+async def api_fetch_data_aggregates(datalogger: str, before: datetime = Query(datetime.now()),
+                                    since: datetime = Query(datetime.now()), span: str | None = "raw"):
+    return get_data_from_csv(datalogger, before, since, span)
 
 
 @app.get("/api/data", response_model=List[DataRecordResponse])
-async def api_fetch_data_raw(query_params: MeasurementQueryParams):
-    return get_data_from_csv(query_params)
+async def api_fetch_data_raw(datalogger: str, before: datetime = Query(datetime.now()),
+                             since: datetime = Query(datetime.now())):
+    return get_data_from_csv(datalogger, before, since, "raw")
